919925befb59ed949f52c719a853ffaf114a986f
Satya Nayak <141466587+satyanayakgit@users.noreply.github.com>
2023-12-02 22:56:48 +0000

    test on codespace

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -57,7 +57,7 @@ const Banner = () => {
           className="w-full px-16 py-6 flex justify-between fixed top-0 z-40 bg-black bg-opacity-25"
         >
           <h1 className="font-bodyFont text-4xl text-white font-extrabold border-2 w-12 text-center">
-            S
+            Sn
           </h1>
           <div
             onClick={() => setMenu(true)}
