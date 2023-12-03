7f98eeec60d16352441e7f0a9d4c6b54101127e1
Satya Nayak <141466587+satyanayakgit@users.noreply.github.com>
2023-12-03 06:17:28 +0530

    Update LeftSide.tsx

--- components/LeftSide.tsx
+++ components/LeftSide.tsx
@@ -10,12 +10,11 @@ const LeftSide = () => {
   return (
     <div className="w-full h-full flex flex-col items-center justify-end gap-4 text-textLight">
       <div className="flex flex-col gap-4">
-        <a href="https://github.com/satyanayakgit" target="_blank">
+        <a href="https://github.com/noorjsdivs" target="_blank">
           <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
             <TbBrandGithub />
           </span>
         </a>
-        {/*
         <a
           href="https://www.youtube.com/channel/UChkOsij0dhgft0GhHRauOAA"
           target="_blank"
@@ -24,16 +23,14 @@ const LeftSide = () => {
             <SlSocialYoutube />
           </span>
         </a>
-  */}
         <a
-          href="www.linkedin.com/in/satyanayakin"
+          href="https://www.linkedin.com/in/noor-mohammad-ab2245193/"
           target="_blank"
         >
           <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
             <SlSocialLinkedin />
           </span>
         </a>
-        {/*}
         <a href="https://www.facebook.com/Noorlalu143/" target="_blank">
           <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
             <SlSocialFacebook />
@@ -44,7 +41,6 @@ const LeftSide = () => {
             <SlSocialInstagram />
           </span>
         </a>
-*/}
       </div>
       <div className="w-[2px] h-32 bg-textDark"></div>
     </div>
