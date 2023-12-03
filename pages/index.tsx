ae87dc6d947229b60481e21f3a9f052367cac6f5
Satya Nayak <141466587+satyanayakgit@users.noreply.github.com>
2023-12-03 06:51:14 +0530

    Update index.tsx  back to og

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,34 +16,6 @@ const Home: NextPage = () => {
         <title>Satya Nayak | Profile</title>
         <link rel="icon" href="/smallLogo.ico" />
       </Head>
- <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
-      <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 1.5 }}
-            className="hidden xl:inline-flex w-32 h-full fixed left-0 bottom-0"
-          >
-            <LeftSide />
-          </motion.div>
-          <div className="h-[88vh] mx-auto p-4">
-            <Banner />
-            <AboutMe />
-            <Offers />
-            <Contact />
-            <Footer />
-          </div>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 1.5 }}
-            className="hidden xl:inline-flex w-32 h-full fixed right-0 bottom-0"
-          >
-        </motion.div>
- </div>    
-
-
-    ==========================
-
       <div className="font-bodyFont">
         <Banner />
         <AboutMe />
